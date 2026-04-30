cc26b7f6b8e4f1880542501e91833ed6f0f0b8c9
Noor Zureikat <noorzureikat@Noors-MacBook-Air-3.local>
2026-04-30 10:58:19 +0200

    added StatsManager and update logic

--- observers.py
+++ observers.py
@@ -21,34 +21,34 @@ class StatsManager:
             "snowboarder": 0
         }
 
-def update(self, event_type, data):
-    if event_type == "rental_wait":
-        self.wait_times["rental"].append(data)
+    def update(self, event_type, data):
+        if event_type == "rental_wait":
+            self.wait_times["rental"].append(data)
 
-    elif event_type == "lift_wait":
-        self.wait_times["lift"].append(data)
+        elif event_type == "lift_wait":
+            self.wait_times["lift"].append(data)
 
-    elif event_type == "cafe_wait":
-        self.wait_times["cafe"].append(data)
+        elif event_type == "cafe_wait":
+            self.wait_times["cafe"].append(data)
 
-    elif event_type == "rental_queue":
-        self.queue_lengths["rental"].append(data)
+        elif event_type == "rental_queue":
+            self.queue_lengths["rental"].append(data)
 
-    elif event_type == "lift_queue":
-        self.queue_lengths["lift"].append(data)
+        elif event_type == "lift_queue":
+            self.queue_lengths["lift"].append(data)
 
-    elif event_type == "cafe_queue":
-        self.queue_lengths["cafe"].append(data)
+        elif event_type == "cafe_queue":
+            self.queue_lengths["cafe"].append(data)
 
-    elif event_type == "run":
-        self.total_runs += 1
+        elif event_type == "run":
+            self.total_runs += 1
 
-    elif event_type == "cafe":
-        self.cafe_visits += 1
+        elif event_type == "cafe":
+            self.cafe_visits += 1
 
-    elif event_type == "fall":
-        self.falls += 1
+        elif event_type == "fall":
+            self.falls += 1
 
-    elif event_type == "visitor_type":
-        if data in self.visitor_types:
-            self.visitor_types[data] += 1
+        elif event_type == "visitor_type":
+            if data in self.visitor_types:
+                self.visitor_types[data] += 1
